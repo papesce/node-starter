1ef3d63575fa4c20573cc77f131e24727875eeb3
Pablo Pesce <papesce@gmail.com>
2017-11-03 09:56:04 -0300

    mocha and chai

---
+++ test/index.test.js
@@ -0,0 +1,9 @@
+var {expect} = require('chai');
+
+describe("Tests", function() {
+    it("should detect malformed JSON strings", function(){
+            //Test Goes Here
+            expect(4+5).equals(9);
+            //expect(traverse(x)).
+    });
+ });
